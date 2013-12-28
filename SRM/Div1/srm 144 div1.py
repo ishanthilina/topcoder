3127275ef8dd86d107fdc1102708dc937bc8d8ff
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2013-12-28 17:25:10 +0530

    Initial commit for srm 144 div1

---
+++ SRM/Div1/srm 144 div1.py
@@ -0,0 +1,10 @@
+class BinaryCode(object):
+	"""Solution for srm 144 div1 of topCoder"""
+	def __init__(self):
+		super(BinaryCode, self).__init__()
+		
+	def decode(self, message):
+		"""
+		message : A string in the format "123210122"
+		"""
+		pass
